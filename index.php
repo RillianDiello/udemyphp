6d9a8e9e1b02a772fd4f8cb73f872ca35d36edfb
Rillian Diello <rilliansf@gmail.com.br>
2017-08-21 01:12:40 -0300

    resolvi o conflito

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "Ola mundo 456";
 
-echo "Ola udemy";
-=======
-echo "Ola mundo 123";
+echo "Ola udemy e Olha Hcod";
 
-echo "Olá Hcode";
->>>>>>> c6ea0e8624fd6f449c43a64cc6efea894cfba09e
 
 ?>
